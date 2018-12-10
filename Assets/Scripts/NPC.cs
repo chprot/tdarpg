eb916c59a6a6b72e4c84e10b0a2f2a2dc2e668ee
Christopher Proteau <christopherproteau@gmail.com>
2018-12-09 19:47:40 -0800

    Add missing files

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : Character {
+
+    // Use this for initialization
+    protected override void StartImpl ()
+    {
+
+	}
+
+
+    protected override void UpdateImpl()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : Character {
+
+    // Use this for initialization
+    protected override void StartImpl()
+    {
+        this.spriteRenderer.sprite = bottomSprite;
+    }
+
+
+    protected override void UpdateImpl()
+    {
+        HandleInput();
+    }
+
+    void HandleInput()
+    {
+        Vector3 playerMovement = Vector3.zero;
+        playerMovement.x = Input.GetAxis("Horizontal");
+        playerMovement.y = Input.GetAxis("Vertical");
+        characterRigidBody.velocity = playerMovement;
+
+        //Vector3 lookDirection = Vector3.zero;
+        lookDirection.x = Input.GetAxis("LookHorizontal");
+        lookDirection.y = Input.GetAxis("LookVertical");
+        if (lookDirection.magnitude == 0f)
+        {
+            lookDirection.x = playerMovement.x;
+            lookDirection.y = playerMovement.y;
+        }
+    }
+}
