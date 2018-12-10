093db006ffc0e236585e8ea931fb90ec573f8482
Christopher Proteau <christopherproteau@gmail.com>
2018-12-09 20:33:12 -0800

    Add comments

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class Player : Character {
 
     // Use this for initialization
@@ -11,23 +10,29 @@ public class Player : Character {
         this.spriteRenderer.sprite = bottomSprite;
     }
 
-
+	// Update is called once per frame
     protected override void UpdateImpl()
     {
+        // Handle any user input
         HandleInput();
     }
 
+    // Handle user input and update player state accordingly
     void HandleInput()
     {
+        // Update character's velocity based on input
         Vector3 playerMovement = Vector3.zero;
         playerMovement.x = Input.GetAxis("Horizontal");
         playerMovement.y = Input.GetAxis("Vertical");
         characterRigidBody.velocity = playerMovement;
 
-        //Vector3 lookDirection = Vector3.zero;
+        // Set the character's look direction based on input
         lookDirection.x = Input.GetAxis("LookHorizontal");
         lookDirection.y = Input.GetAxis("LookVertical");
-        if (lookDirection.magnitude == 0f)
+        lookDirection.y = 0.0f;
+
+        // Set look direction to player movement forward vector, if no other look input set
+        if (lookDirection.magnitude == 0.0f)
         {
             lookDirection.x = playerMovement.x;
             lookDirection.y = playerMovement.y;
